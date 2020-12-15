9f886d247560f27bf529c403ee3304658bef0f2a
AlexDyatlov <aleks78.gooogle@gmail.com>
2020-12-15 23:10:22 +0300

    added layout for PC

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # HABAT-test
 
 Тестовое задание - вёрстка страницы.
+
+<p align="center">
+  <img src="https://github.com/AlexDyatlov/HABAT/raw/master/build/images/readme.png">
+</p>
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 $(function(){
 
+  $('.services__inner').slick({
+    dots: true,
+    prevArrow: '<button class="slider-arrows slider-arrows-prev"><img src="images/btnprev.svg" alt=""></button>',
+    nextArrow: '<button class="slider-arrows slider-arrows-next"><img src="images/btnnext.svg" alt=""></button>',
+  });
+
+  $('.tab').on('click', function(e){
+    e.preventDefault();
+
+    $($(this).siblings()).removeClass('tab--active');
+    $($(this).closest('.tabs-wrapper').siblings().find('div')).removeClass('tabs-content--active');
+
+    $(this).addClass('tab--active');
+    $($(this).attr('href')).addClass('tabs-content--active');
+  }); 
+
 });
